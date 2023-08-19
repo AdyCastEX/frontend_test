fbbedb0270a3eb75768c3cb0ee00e08356befaa8
renzrph <renzestorrph@gmail.com>
2023-08-20 01:47:48 +0800

    Fix user info displayed in modal

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -24,7 +24,7 @@ const Gallery = ({ users }: GalleryProps) => {
   const handleModalOpen = (id: number) => {
     const user = usersList.find((item) => item.id === id) || null;
 
-    if(user) {
+    if (user) {
       setSelectedUser(user);
       setIsModalOpen(true);
     }
@@ -91,11 +91,11 @@ const Gallery = ({ users }: GalleryProps) => {
                   <div className="name">
                     {selectedUser.name} ({selectedUser.username})
                   </div>
-                  <div className="field">
+                  <div className="fields">
                     <FaLocationDot className="icon" />
-                    <div className="data">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
+                    <div className="value">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
                   </div>
-                  <div className="field">
+                  <div className="fields">
                     <FaPhone className="icon" />
                     <div className="value">{selectedUser.phone}</div>
                   </div>
