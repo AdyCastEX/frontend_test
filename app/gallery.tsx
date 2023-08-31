24d6a138f67e12b8eddef61a8b6da22806ff2c5d
kiembully <kimklintonvale@gmail.com>
2023-08-31 12:01:53 +0800

    FIX: [Hard][Feature] Data fetching from API
    Fetched data from a public API and rendered them in the application

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Avatar from "boring-avatars";
 import {
   FaRegCircleXmark,
@@ -13,20 +13,20 @@ import Modal from "./modal";
 
 import { User } from "./types/user";
 
-export type GalleryProps = {
-  users: User[];
-};
-const Gallery = ({ users }: GalleryProps) => {
-  const [usersList, setUsersList] = useState(users);
+
+const Gallery = () => {
+  const [usersList, setUsersList] = useState([]);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleModalOpen = (id: number) => {
-    const user = usersList.find((item) => item.id === id) || null;
+    if (usersList) {
+      const user = usersList.find((item: any) => item.id === id) || null;
 
-    if(user) {
-      setSelectedUser(user);
-      setIsModalOpen(true);
+      if(user) {
+        setSelectedUser(user);
+        setIsModalOpen(true);
+      }
     }
   };
 
@@ -35,11 +35,36 @@ const Gallery = ({ users }: GalleryProps) => {
     setIsModalOpen(false);
   };
 
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('Loading . . .')
+  const getUsers = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      setUsersList(data.users);
+      setError(false)
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      setErrorMessage("Error fetching user data.")
+      setError(true)
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
   return (
     <div className="user-gallery">
       <h1 className="heading">Users</h1>
       <div className="items">
-        {usersList.map((user, index) => (
+        {loading || error
+          ? <h1>{errorMessage}</h1>
+          : usersList.map((user: any, index) => (
           <div
             className="item user-card"
             key={index}
@@ -48,13 +73,13 @@ const Gallery = ({ users }: GalleryProps) => {
             <div className="body">
               <Avatar
                 size={96}
-                name={user.name}
+                name={user.firstName}
                 variant="marble"
                 colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
               />
             </div>
             <div className="info">
-              <div className="name">{user.name}</div>
+              <div className="name">{user.firstName}</div>
               <div className="company">{user.company.name}</div>
             </div>
           </div>
@@ -77,7 +102,7 @@ const Gallery = ({ users }: GalleryProps) => {
                   <div className="avatar">
                     <Avatar
                       size={240}
-                      name={selectedUser.name}
+                      name={selectedUser.firstName}
                       variant="marble"
                       colors={[
                         "#92A1C6",
@@ -89,11 +114,11 @@ const Gallery = ({ users }: GalleryProps) => {
                     />
                   </div>
                   <div className="name">
-                    {selectedUser.name} ({selectedUser.username})
+                    {selectedUser.firstName} ({selectedUser.username})
                   </div>
                   <div className="field">
                     <FaLocationDot className="icon" />
-                    <div className="data">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
+                    <div className="data">{`${selectedUser.address.address}, ${selectedUser.address.state}, ${selectedUser.address.city}`}</div>
                   </div>
                   <div className="field">
                     <FaPhone className="icon" />
@@ -106,7 +131,7 @@ const Gallery = ({ users }: GalleryProps) => {
                   <div className="company">
                     <div className="name">{selectedUser.company.name}</div>
                     <div className="catchphrase">
-                      {selectedUser.company.catchPhrase}
+                      {selectedUser.company.title}
                     </div>
                   </div>
                 </div>
